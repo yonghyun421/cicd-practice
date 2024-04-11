9532b0be54429e55194a690d1610894e0e96eedc
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-04-11 21:07:15 +0900

    chore: add format and lint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { decrease, increase } from "./utils";
-import "./App.css";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { decrease, increase } from './utils';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [greet, setGreet] = useState("");
+  const [greet, setGreet] = useState('');
 
   useEffect(() => {
-    axios.get("http://localhost:8000/api/greet").then((res) => {
-      setGreet(res.data?.message || "");
+    axios.get('http://localhost:8000/api/greet').then(res => {
+      setGreet(res.data?.message || '');
     });
   }, []);
 
   return (
     <div className="card">
       <div>{count}</div>
-      <button onClick={() => setCount((count) => increase(count))}>증가</button>
-      <button onClick={() => setCount((count) => decrease(count))}>감소</button>
+      <button onClick={() => setCount(count => increase(count))}>증가</button>
+      <button onClick={() => setCount(count => decrease(count))}>감소</button>
       {greet && <p data-testid="greet">{greet}</p>}
     </div>
   );
--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,50 +1,50 @@
-import { describe, it, afterEach, expect } from "vitest";
-import { cleanup, render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
-import App from "./App";
+import { describe, it, afterEach, expect } from 'vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import App from './App';
 
 afterEach(() => {
   cleanup();
 });
 
-describe("App.js", () => {
-  it("처음에 숫자를 0으로 보여줘요.", () => {
+describe('App.js', () => {
+  it('처음에 숫자를 0으로 보여줘요.', () => {
     render(<App />);
 
-    expect(screen.getByText("0")).toBeInTheDocument();
+    expect(screen.getByText('0')).toBeInTheDocument();
   });
 
-  it("증가버튼을 클릭하면 숫자가 올라가요", async () => {
+  it('증가버튼을 클릭하면 숫자가 올라가요', async () => {
     render(<App />);
 
-    const countElement = screen.getByText("0");
-    const increaseButtonElement = screen.getByText("증가");
+    const countElement = screen.getByText('0');
+    const increaseButtonElement = screen.getByText('증가');
 
     await userEvent.click(increaseButtonElement);
     await userEvent.click(increaseButtonElement);
     await userEvent.click(increaseButtonElement);
 
-    expect(countElement).toHaveTextContent("3");
+    expect(countElement).toHaveTextContent('3');
   });
 
-  it("감소버튼을 클릭하면 숫자가 내려가요", async () => {
+  it('감소버튼을 클릭하면 숫자가 내려가요', async () => {
     render(<App />);
 
-    const countElement = screen.getByText("0");
-    const decreaseButtonElement = screen.getByText("감소");
+    const countElement = screen.getByText('0');
+    const decreaseButtonElement = screen.getByText('감소');
 
     await userEvent.click(decreaseButtonElement);
     await userEvent.click(decreaseButtonElement);
 
-    expect(countElement).toHaveTextContent("-2");
+    expect(countElement).toHaveTextContent('-2');
   });
 
-  it("방문 후 잠시 뒤 인사말이 보여져요", async () => {
+  it('방문 후 잠시 뒤 인사말이 보여져요', async () => {
     render(<App />);
 
-    const greetElement = await screen.findByTestId("greet");
+    const greetElement = await screen.findByTestId('greet');
 
-    expect(greetElement).toHaveTextContent("Hello");
+    expect(greetElement).toHaveTextContent('Hello');
   });
 });
